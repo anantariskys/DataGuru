2816573ad5b9b4795c5b2d84c46f5d7dd670b378
anantariskys <gamersindo555@gmail.com>
2024-08-14 12:49:20 +0700

    Feat : create component InfoCard

---
+++ src/components/InfoCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title = '';
+    export let subtitle = '';
+  </script>
+  
+  <div class="flex flex-col items-center">
+    <h5 class="text-[#6D6D6D] text-xl font-semibold">{title}</h5>
+    <slot name="value" /> 
+    <p class="text-sm text-gray-500">{subtitle}</p>
+  </div>
+  
