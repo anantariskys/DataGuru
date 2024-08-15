6c4270b3da05250af4447b18676e7b7fa82c4f5a
anantariskys <gamersindo555@gmail.com>
2024-08-15 09:04:15 +0700

    fix [ReviewCard] : add gap between content and author

--- src/components/ReviewCard.svelte
+++ src/components/ReviewCard.svelte
@@ -1,13 +1,15 @@
 <script>
-    import Quotes from "../assets/Quotes.png";
-    export let content;
-    export let author;
-  </script>
-  
-  <div
-    class="w-96 flex-shrink-0 px-10 py-4 space-y-3 rounded-2xl shadow-md text-white bg-gradient-to-b from-[#7052FF] to-[#A368FE] mx-4"
-  >
-    <img src={Quotes} draggable="false" alt="quotes" />
+  import Quotes from "../assets/Quotes.png";
+  export let content;
+  export let author;
+</script>
+
+<div
+  class="w-96 flex-shrink-0 px-10 py-4 space-y-3 rounded-2xl shadow-md text-white bg-gradient-to-b from-[#7052FF] to-[#A368FE] mx-4 justify-between flex flex-col"
+>
+  <div class="space-y-2">
+    <img src={Quotes} draggable="false" class="w-8 aspect-auto" alt="quotes" />
     <p class="">{content}</p>
-    <p class="font-bold">~ {author}</p>
   </div>
+  <p class="font-bold">~ {author}</p>
+</div>
